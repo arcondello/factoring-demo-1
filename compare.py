7595a94802ee4fd222c18a4cbdd1d97c1977e937
Bradley Ellert <bellert@dwavesys.com>
2018-04-23 10:22:13 -0700

    Do embedding manually

--- compare.py
+++ compare.py
@@ -1,4 +1,5 @@
 import pickle
+import shelve
 import time
 
 from factoring.interfaces import get_factor_bqm, submit_factor_bqm, postprocess_factor_response
@@ -10,17 +11,19 @@ if __name__ == '__main__':
 
     for trial in range (1000):
         bqm = get_factor_bqm(P)
-        response = submit_factor_bqm(bqm)
+        response, embedding = submit_factor_bqm(bqm)
         output = postprocess_factor_response(response, P)
-        with open("pickle/orig%s" % time.time(), "wb") as f:
-            pickle.dump(output, f)
+        shelf = shelve.open("pickle/embedding%s" % time.time(), "wb")
+        shelf['output'] = output
+        shelf['embedding'] = embedding
+        shelf.close()
 
-        output = factor(P)
-        with open("pickle/fixed%s" % time.time(), "wb") as f:
-            pickle.dump(output, f)
+        # output = factor(P)
+        # with open("pickle/fixed%s" % time.time(), "wb") as f:
+        #     pickle.dump(output, f)
 
 # for filename in os.listdir('.'):
 #     if filename.startswith('fixed'):
 #     with open(filename) as f:
-#                 stuff = pickle.load(f)
-#     print([x['percentageOfOccurrences'] for x in stuff['results'] if x['valid']])
+#         stuff = pickle.load(f)
+#         print([x['percentageOfOccurrences'] for x in stuff['results'] if x['valid']])
